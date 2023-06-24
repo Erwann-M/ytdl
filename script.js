682bfcaaf5e045f0892e7a0f6e28ffb0acf505af
Erwann-M <rwnnmrtn@gmail.com>
2023-06-24 14:48:56 +0200

    add readme and add input for custom name of music

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 var convertBtn = document.querySelector('.convert-button');
 var URLinput = document.querySelector('.URL-input');
+var NAMEinput = document.querySelector('.music_name');
 convertBtn.addEventListener('click', () => {
     console.log(`URL: ${URLinput.value}`);
-    sendURL(URLinput.value);
+    sendURL(URLinput.value, NAMEinput.value);
 });
-function sendURL(URL) {
-    window.location.href = `http://localhost:5500/download?URL=${URL}`;
+function sendURL(URL, NAME) {
+    window.location.href = `https://ytdl.erwann-martin.fr/download?URL=${URL}&NAME=${NAME}`;
 }
--- server/index.js
+++ server/index.js
@@ -3,12 +3,14 @@ const cors = require('cors');
 const ytdl = require('ytdl-core');
 const app = express();
 app.use(cors());
-app.listen(5500, () => {
-    console.log('Server Works !!! At port 4000');
+app.listen(200, () => {
+    console.log('Server Works !!! At port 5500');
 });
 app.get('/download', (req,res) => {
 var URL = req.query.URL;
-res.header('Content-Disposition', 'attachment; filename="music.mp3"');
+var NAME = req.query.NAME;
+var fileName = `${NAME}.mp3`;
+res.header('Content-Disposition', `attachment; filename=`+fileName);
 ytdl(URL, {
     format: 'mp3',
     filter: 'audioonly'
